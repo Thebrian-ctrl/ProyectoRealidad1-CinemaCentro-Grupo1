012badabf3a4f07991914c2c0da48c76733acb07
jonatan <david.joni2401@gmail.com>
2025-10-28 20:55:59 -0300

    se provo el metodo detalleticket

--- CinemaCentro/CinemaCentro-Grupo1/src/Persistencia/DetalleTicketData.java
+++ CinemaCentro/CinemaCentro-Grupo1/src/Persistencia/DetalleTicketData.java
@@ -6,6 +6,8 @@
 package Persistencia;
 
 import Modelo.DetalleTicket;
+import Modelo.Funcion;
+import Modelo.Lugar;
 import Modelo.MyConexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -53,15 +55,18 @@ public class DetalleTicketData {
     }
 
     public void actualizarDetalleTicket(DetalleTicket d) {
-        String query = "UPDATE DetalleTicket SET idFuncion = ?, idLugar = ?, cantidad = ?, subtotal = ? WHERE idDetalleTicket = ?";
+        String query = "UPDATE detalleticket SET idFuncion = ?, idLugar = ?, cantidad = ?, subtotal = ? WHERE idDetalleTicket = ?";
+        
+        
 
         try {
-            PreparedStatement ps = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+            PreparedStatement ps = conn.prepareStatement(query);
 
             ps.setInt(1, d.getFuncion().getIdFuncion());
             ps.setInt(2, d.getLugar().getIdLugar());
             ps.setInt(3, d.getCantidad());
             ps.setDouble(4, d.getSubtotal());
+            ps.setInt(5, d.getIdDetalleTicket());
 
             int actualizado = ps.executeUpdate();
 
@@ -70,33 +75,68 @@ public class DetalleTicketData {
             } else {
                 JOptionPane.showMessageDialog(null, "No se pudo actualizar el detalleticket");
             }
+            ps.close();
 
         } catch (Exception e) {
+           e.printStackTrace();
             JOptionPane.showMessageDialog(null, "Error al actualizar el detalleticket ");
+            
         }
 
     }
-    
-    public void eliminarDetalleTicket(int idDetalleTicket){
-       String query = "DELETE FROM detalleticket WHERE idDetalleTicket = ?";
-       
-       
+
+    public void eliminarDetalleTicket(int idDetalleTicket) {
+        String query = "DELETE FROM detalleticket WHERE idDetalleTicket = ?";
+
         try {
-            PreparedStatement ps= conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
-            
+            PreparedStatement ps = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+
             ps.setInt(1, idDetalleTicket);
             int eliminado = ps.executeUpdate();
-            
-            
-            if(eliminado == 1) {
+
+            if (eliminado == 1) {
                 JOptionPane.showMessageDialog(null, "DetalleTicket eliminado correctamente");
             } else {
                 JOptionPane.showMessageDialog(null, "No se encontró el detalleticket con ese ID");
-            }
-            
-        }catch(Exception e){
+            } ps.close();
+
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Error al eliminar el detalleticket");
         }
-      
-  }
+
+    }
+
+    public DetalleTicket buscarDetalleTicket(int id) {
+        String query = "SELECT * FROM detalleticket WHERE idDetalleTicket = ?";
+
+        DetalleTicket detalle = null;
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                detalle = new DetalleTicket();
+
+                detalle.setIdDetalleTicket(rs.getInt("idDetalleTicket"));
+                detalle.setCantidad(rs.getInt("cantidad"));
+                detalle.setSubtotal(rs.getDouble("subtotal"));
+                Funcion funcion = new Funcion();
+                funcion.setIdFuncion(rs.getInt("idFuncion"));
+                detalle.setFuncion(funcion);
+
+                Lugar lugar = new Lugar();
+                lugar.setIdLugar(rs.getInt("idLugar"));
+            } else {
+                JOptionPane.showMessageDialog(null, "No se encontro el detalleticket");
+            }
+            ps.close();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al buscar el detalleticket");
+        }
+        return detalle;
+    }
 }
--- CinemaCentro/CinemaCentro-Grupo1/src/cinemacentro/grupo1/CinemaCentroGrupo1.java
+++ CinemaCentro/CinemaCentro-Grupo1/src/cinemacentro/grupo1/CinemaCentroGrupo1.java
@@ -6,16 +6,19 @@
 package cinemacentro.grupo1;
 
 import Modelo.Comprador;
+import Modelo.DetalleTicket;
 import Modelo.Funcion;
 import Modelo.Lugar;
 import Modelo.Pelicula;
 import Modelo.Sala;
+import Modelo.TicketCompra;
 import Persistencia.CompradorData;
 import Persistencia.DetalleTicketData;
 import Persistencia.FuncionData;
 import Persistencia.LugarData;
 import Persistencia.PeliculaData;
 import Persistencia.SalaData;
+import Persistencia.TicketCompraData;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -34,54 +37,43 @@ public class CinemaCentroGrupo1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
-          // Pelicula peli1 = new Pelicula("El señor de los anillos", "Chritopher Nolan", "Brad pitt", "Estado Unidos", "Aventuras", LocalDate.of(2005, Month.MARCH, 15), true);
+
+//        Pelicula peli1 = new Pelicula("El señor de los anillos", "Chritopher Nolan", "Brad pitt", "Estado Unidos", "Aventuras", LocalDate.of(2005, Month.MARCH, 15), true);
 //        
 //        PeliculaData pelidata = new PeliculaData();
 //        
 //        pelidata.guardarPelicula(peli1);
 //        
-         // Pelicula peli2 = new Pelicula("Troya", "Arce Brayan", "Brad pitt, Orlando blum", "Estados Unidos", "Historia", LocalDate.of(2000, Month.MARCH, 16), true);
+        // Pelicula peli2 = new Pelicula("Troya", "Arce Brayan", "Brad pitt, Orlando blum", "Estados Unidos", "Historia", LocalDate.of(2000, Month.MARCH, 16), true);
 //        
 //        pelidata.guardarPelicula(peli2);
-        
         //-------------Probamos el metodo para buscar la pelicula por el nombre-------------------
-        
 //       Pelicula buscada = pelidata.buscarPelicula("El señor de los anillos");
 //       
 //       if(buscada != null){
 //           System.out.println("Pelicula encontrada: " + buscada.toString());
 //       
 //       }
-
         //------------Probamos el metodo para listar las peliculas------------------
 //        List<Pelicula> peliculaEnCartelera = pelidata.listarPeliculasCartelera();
 //        
 //        for (Pelicula peli : peliculaEnCartelera) {
 //            System.out.println(peli);
 //        }
-
         //---------------Prueba de dar de baja a una pelicula (cartelera)-------------
-        
         //pelidata.darBajaCartelera(1);
-        
 //        List<Pelicula> peliculaEnCartelera = pelidata.listarPeliculasCartelera();
 //        
 //       for (Pelicula peli : peliculaEnCartelera) {
 //            System.out.println(peli);
 //        }
-       
-       //--------------Prueba dar de alta a una pelicula-------------
-       
+        //--------------Prueba dar de alta a una pelicula-------------
 //       pelidata.darAltaCartelera(1);
 //       List<Pelicula> peliculaEnCartelera = pelidata.listarPeliculasCartelera();
 //        
 //       for (Pelicula peli : peliculaEnCartelera) {
 //            System.out.println(peli);
 //        }
-
-        
         //-----------------Prueba eliminar pelicula-----------------
 //        pelidata.eliminarPelicula(2);
 //        List<Pelicula> peliculaEnCartelera = pelidata.listarPeliculasCartelera();
@@ -89,14 +81,7 @@ public class CinemaCentroGrupo1 {
 //       for (Pelicula peli : peliculaEnCartelera) {
 //            System.out.println(peli);
 //        }
-
-
-
-
-
-
 //-------------------------------<<======COMPRADOR DATA=====>>!!---------------
-
 //
 //    CompradorData compradorData = new CompradorData();
 //    
@@ -105,8 +90,6 @@ public class CinemaCentroGrupo1 {
 //    Comprador compra1 = new Comprador (41221751, "Gimenez Paula" , "123", "Mercado Pago", LocalDate.of(1998,06,06));
 //    
 //    compradorData.guardarComprador(compra1);
-    
-    
 //    Comprador compra2 = new Comprador (42278291,"Arce Brayan", "231","Efectivo",LocalDate.of(1999,12,16));
 //    
 //    compradorData.guardarComprador(compra2);
@@ -138,8 +121,7 @@ public class CinemaCentroGrupo1 {
 //     for(Comprador c: lista){
 //         System.out.println(c);
 //     }
-    
-    //----------------ACTUALIZAR COMPRADOR--------------
+        //----------------ACTUALIZAR COMPRADOR--------------
 //    Comprador compraBuscar= compradorData.buscarComprador(41221751);
 //    
 //    compraBuscar.setMedioDePago("Debito");
@@ -160,22 +142,16 @@ public class CinemaCentroGrupo1 {
 //     for(Comprador c: lista){
 //         System.out.println(c);
 //     }  
-
-
-
-
-
-//       SalaData salaData = new SalaData();
+//        SalaData salaData = new SalaData();
 //
 ////        // -----------Guardar nueva sala--------
-       // Sala sala1 = new Sala(1, true, 200, true); 
-//       salaData.guardarSala(sala1);
+//        Sala sala1 = new Sala(1, true, 200, true);
+//        salaData.guardarSala(sala1);
 //      System.out.println("Sala guardada: " + sala1);
 //       Sala sala2 = new Sala(2, true, 180, true); 
 //      salaData.guardarSala(sala2);
 //       Sala sala3 = new Sala(3, true, 400, true); 
 //       salaData.guardarSala(sala3);
-
 //       // ----- Buscar sala por ID-------------
 //       Sala salaBuscada = salaData.buscarSala(4);
 //       if (salaBuscada != null) {
@@ -207,35 +183,28 @@ public class CinemaCentroGrupo1 {
 //       for (Sala s : todasSalas2) {
 //            System.out.println(s);
 //       }
-       
-       // FuncionData funcionData = new FuncionData();
-        
-       // LugarData lugarData = new LugarData();
-        //---------------Prueba de cargar una funcion-----------------
+//        FuncionData funcionData = new FuncionData();
+//
+//        LugarData lugarData = new LugarData();
+//        //---------------Prueba de cargar una funcion-----------------
 //        Pelicula pelicula = new Pelicula();
-//        
+//
 //        pelicula.setIdPelicula(1);
-//        
+//
 //        Sala sala = new Sala();
-//        
+//
 //        sala.setIdSala(1);
-//        
+//
 //        List<Lugar> lugars = new ArrayList();
-//        
-//       
-//        
+//
 //        Funcion funcion1 = new Funcion(pelicula, "espanol", false, true, LocalDateTime.of(2025, 12, 16, 16, 30), LocalDateTime.of(2025, 12, 16, 18, 30), lugars, sala, 8000);
-//       
-//        
-//        
-//         funcionData.guardarFuncion(funcion1);
-
-
-//------------------------PROBAMOS GUARDAR UN LUGAR --------------
-//        
-//    char[] filas = {'A', 'B', 'C'};
-//       int asientosPorFila = 3;
-//       
+//
+//        funcionData.guardarFuncion(funcion1);
+//
+////------------------------PROBAMOS GUARDAR UN LUGAR --------------
+//        char[] filas = {'A', 'B', 'C'};
+//        int asientosPorFila = 3;
+//
 //        for (char f : filas) {
 //            for (int i = 1; i <= asientosPorFila; i++) {
 //                Lugar lugui = new Lugar();
@@ -243,15 +212,11 @@ public class CinemaCentroGrupo1 {
 //                lugui.setNum(i);
 //                lugui.setEstado(true);
 //                lugui.setFuncion(funcion1);
-//                
+//
 //                lugarData.guardarLugar(lugui);
 //                lugars.add(lugui);
 //            }
 //        }
-        
-        
-       
-       
 //       Funcion funcion2 = funcionData.buscarFuncion(13);
 //       
 //        System.out.println("Lugares asignados");
@@ -260,7 +225,6 @@ public class CinemaCentroGrupo1 {
 //            System.out.println("Fila: " + lugar.getFila() + " num: " + lugar.getNum() + " estado: " + (lugar.isEstado() ? "Libre" : "Ocupado"));
 //            
 //        }
-       
 //       //--------------------Prueba de actualizar una funcion------------------
 //       
 //       Funcion funcion = new Funcion();
@@ -271,8 +235,7 @@ public class CinemaCentroGrupo1 {
 //       
 //       funcionData.actualizarFuncion(funcion);
 //       
-       
-       //-------------Prueba para probar el listar funciones---------------
+        //-------------Prueba para probar el listar funciones---------------
 //        System.out.println("Lista de funciones: ");
 //        List<Funcion> listaFunciones = funcionData.listarFuncion();
 //        
@@ -288,8 +251,6 @@ public class CinemaCentroGrupo1 {
 //        }
 //        
 //    }
-    
-        
 //        
 //        List<Lugar> buscaLugares = lugarData.buscarLugaresPorFuncion(12);
 //        
@@ -298,13 +259,8 @@ public class CinemaCentroGrupo1 {
 //            System.out.print(" fila: " + busca.getFila());
 //            System.out.println(" disponible: " + busca.isEstado());
 //        }
-
-
-
-
-
 //------------ PRUEBA DE LUGAR SIN ASOCIAR UNA FUNCION -------------
-  LugarData lugarData = new LugarData();  
+//  LugarData lugarData = new LugarData();  
 //    
 //    
 //    Lugar lugar = new Lugar();
@@ -318,11 +274,8 @@ public class CinemaCentroGrupo1 {
 //    
 //    System.out.println("Lugar guardado"+lugar);
 //    
-
-
-    //-------------- actualizar un lugar-----------
-    
-    Lugar lugar = new Lugar();
+        //-------------- actualizar un lugar-----------
+        Lugar lugar = new Lugar();
 //   lugar=lugarData.buscarLugarPorId(4);
 //     lugar.setEstado(false);
 //   ------------ dar de baja y alta un ligar------------
@@ -339,9 +292,50 @@ public class CinemaCentroGrupo1 {
 //                           " | Estado: " + (l.isEstado() ? "Libre" : "Ocupado"));
 //        }
 //
-
 //-------------- eliminar un lugar----------------
-    //lugarData.eliminarlugar(4);
+        //lugarData.eliminarlugar(4);
+//----------- Probamos el DetalleTicket --------------
+//        DetalleTicketData detalleticketdata = new DetalleTicketData();
+//
+//        DetalleTicket dticket = new DetalleTicket();
+//
+////        dticket.setIdDetalleTicket(1);
+////        System.out.println(dticket);
+//        FuncionData funciondata = new FuncionData();
+//
+//        Funcion f = new Funcion();
+////    
+//        Lugar l = new Lugar();
+//        l.setIdLugar(10);
+////    
+//        f.setIdFuncion(2);
+////    
+//        dticket.setFuncion(f);
+////    
+//        dticket.setLugar(l);
+//
+//        dticket.setCantidad(1);
+//
+//        dticket.setSubtotal(5000);
+//    
+//        detalleticketdata.guardarDetalleTicket(dticket);
+//        dticket.setSubtotal(3000);
+//    --------Para actualizar el detalleticket tenemos que pasar la funcion y el lugar parciandola----------
+//        detalleticketdata.actualizarDetalleTicket(dticket);
+// ------------- Se probo el eliminar
+//        detalleticketdata.eliminarDetalleTicket(1);
+
+
+//          TicketCompraData ticket = new TicketCompraData();
+//          
+//          TicketCompra t = new TicketCompra();
+//          
+//          Comprador comprador = new Comprador();
+          
+          
+          
+
+
     }
-   
+
 }
